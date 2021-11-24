30db1c1ec3b4064ffbc9c5986b6dcd0a2060120a
Andrey_Poltorak <79004862861@yandex.ru>
2021-11-24 16:33:38 +0300

    'lesson_2'

---
+++ Lesson_2/Timer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+private:
+    using clock_t = std::chrono::high_resolution_clock;
+    using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+    std::string m_name;
+    std::chrono::time_point<clock_t> m_beg;
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+    }
+
+public:
+    Timer() : m_beg(clock_t::now()) { }
+    Timer(std::string name) : m_name(name), m_beg(clock_t::now()) { }
+
+    void start(std::string name) {
+        m_name = name;
+        m_beg = clock_t::now();
+    }
+    void print() const {
+        std::cout << m_name << ":\t" << elapsed() * 1000 << " ms" << '\n';
+    }
+};
+
